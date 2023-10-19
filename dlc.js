59e2067213c69a797681e42bfd039cf94c51ef93
Arsalan Kazmi <sonicspeed848@gmail.com>
2023-10-19 19:17:09 +0100

    Revamped the DLC page
    It's now dynamically populated using JavaScript and a JSON table.
    There's also a satirical "checkout" page.
    I thought it was funny.

---
+++ dlc.js
@@ -0,0 +1,50 @@
+// Function to fetch and render DLC items
+async function renderDlcItems() {
+    const response = await fetch('dlc-items.json');
+    const dlcItems = await response.json();
+    const container = document.getElementById('dlc-container');
+
+    const categories = new Set(dlcItems.map(item => item.category));
+
+    categories.forEach(category => {
+        const categoryHeader = document.createElement('h2');
+        categoryHeader.id = category.toLowerCase();
+        categoryHeader.textContent = category;
+        container.appendChild(categoryHeader);
+
+        dlcItems
+            .filter(item => item.category === category)
+            .forEach(item => {
+                const dlcElement = document.createElement('a');
+                dlcElement.setAttribute('href', `checkout.html?name=${item.name}`);
+                dlcElement.id = 'box-link';
+
+                const divElement = document.createElement('div');
+
+                const h4Element = document.createElement('h4');
+                h4Element.id = 'title';
+                h4Element.textContent = item.name;
+
+                const dateP = document.createElement('p');
+                dateP.textContent = `Updated ${item.date}`;
+
+                const descriptionP = document.createElement('p');
+                descriptionP.innerHTML = item.description;
+
+                const compatibilityP = document.createElement('p');
+                compatibilityP.textContent = `Compatible with ${item.compatibility}.`;
+
+                divElement.appendChild(h4Element);
+                divElement.appendChild(dateP);
+                divElement.appendChild(descriptionP);
+                divElement.appendChild(compatibilityP);
+
+                dlcElement.appendChild(divElement);
+
+                container.appendChild(dlcElement);
+            });
+    });
+}
+
+// Call the function to render DLC items
+renderDlcItems();
